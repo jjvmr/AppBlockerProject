d2df966a1adcd19a5595df1a79d81af6440160cf
JJRR <jjvmamanirojas@gmail.com>
2025-06-29 14:53:22 -0600

    Updated tracker.py to get histories specifically from Chrome and to match timestamps in UTC.

--- tracker.py
+++ tracker.py
@@ -1,19 +1,18 @@
-from browser_history import get_history
-from datetime import datetime, timedelta
+from browser_history.browsers import Chrome
+from datetime import datetime, timedelta, timezone
 
 def time_spent_on_domain(domain: str, hours=24) -> int:
     """ Estimate time spent on domain in the last N hours (in seconds) """
-    outputs = get_history()
-    entries = outputs.histories()    #List of *datetime, URL)
-    now = datetime.now()
+    entries = Chrome().fetch_history().histories    #List of (datetime, URL)
+    now = datetime.now(timezone.utc)
     cutoff = now - timedelta(hours=hours)
     time_spent = 0
 
-    for entry in entries:
-        timestamp, url = entry
-        if domain in url and timestamp > cutoff:
+    for timestamp, url, title in entries:
+        ts_utc = timestamp.astimezone(timezone.utc)
+        if domain in url and ts_utc > cutoff:
             time_spent += 300 # rough estimate: 4 mins per visit
-            print(f"Time spent on site: {timestamp} {url}")
+            print(f"Time spent on {title}: {timestamp} {url}")
     return time_spent
 
 if __name__ == "__main__":
